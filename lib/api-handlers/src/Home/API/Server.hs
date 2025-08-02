80373665f72bf840f962211d72b6d2f2a91759db
Michael B. Gale <github@michael-gale.co.uk>
2025-08-02 15:05:08 +0100

    Move API server implementation to `api-handlers` library

---
+++ lib/api-handlers/src/Home/API/Server.hs
@@ -0,0 +1,41 @@
+-- | Implements the API server.
+module Home.API.Server (
+    API,
+    runApiServer
+) where
+
+--------------------------------------------------------------------------------
+
+import Control.Monad.Logger ( runStdoutLoggingT )
+import Control.Monad.IO.Class
+
+import Data.Maybe ( fromMaybe )
+import Data.Text
+
+import Servant
+
+import Network.Wai.Handler.Warp ( run )
+
+import Home.API
+import Home.API.Config
+import Home.Db
+
+--------------------------------------------------------------------------------
+
+-- | An implementation of `API`.
+server :: Server API
+server = pure 5
+
+-- | A WAI `Application` for `server`.
+app :: Application
+app = serve api server
+
+-- | `runApiServer` starts the API on the port given by @cfg@.
+runApiServer :: ApiConfig Text -> IO ()
+runApiServer cfg =
+    runStdoutLoggingT $
+    withDatabase (apiPostgres cfg) $ \_ -> do
+        liftIO $ run port app
+        where port = fromMaybe 8080 (apiPort cfg)
+
+--------------------------------------------------------------------------------
--- lib/api/src/Home/API.hs
+++ lib/api/src/Home/API.hs
@@ -2,47 +2,21 @@
 -- | Implements the API.
 module Home.API (
     API,
-    runApiServer
+    api
 ) where
 
 --------------------------------------------------------------------------------
 
-import Control.Monad.IO.Class
-import Control.Monad.Logger
-
-import Data.Maybe
-import Data.Text
-
 import Servant
-
-import Network.Wai.Handler.Warp ( run )
-
-import Home.API.Config
-import Home.Db
 
 --------------------------------------------------------------------------------
 
 -- | The API as a type.
-type API = Get '[JSON] Int
+type API
+    = Get '[JSON] Int
 
 -- | A term-level proxy for `API`.
 api :: Proxy API
 api = Proxy
-
--- | An implementation of `API`.
-server :: Server API
-server = pure 5
-
--- | A WAI `Application` for `server`.
-app :: Application
-app = serve api server
-
--- | `runApiServer` starts the API on the port given by @cfg@.
-runApiServer :: ApiConfig Text -> IO ()
-runApiServer cfg =
-    runStdoutLoggingT $
-    withDatabase (apiPostgres cfg) $ \dbPool -> do
-        liftIO $ run port app
-        where port = fromMaybe 8080 (apiPort cfg)
 
 --------------------------------------------------------------------------------
