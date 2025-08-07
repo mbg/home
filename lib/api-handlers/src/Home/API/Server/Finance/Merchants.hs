81906d963f6eb7f53d9bf4d9754f8e2355fa8182
Michael B. Gale <github@michael-gale.co.uk>
2025-08-08 00:34:03 +0100

    Check for identical name matches before creating a `Merchant`

--- lib/api-handlers/src/Home/API/Server/Finance/Merchants.hs
+++ lib/api-handlers/src/Home/API/Server/Finance/Merchants.hs
@@ -5,6 +5,8 @@ module Home.API.Server.Finance.Merchants (
 
 --------------------------------------------------------------------------------
 
+import Data.Text ( strip )
+
 import Database.Esqueleto.Experimental
 
 import Home.API.Finance.Merchants
@@ -25,6 +27,14 @@ merchantById :: Key Db.Merchant -> SqlQuery (SqlExpr (Entity Db.Merchant))
 merchantById merchantId = do
     m <- from $ table @Db.Merchant
     where_ $ m ^. Db.MerchantId ==. val merchantId
+    pure m
+
+-- | `merchantByName` @name@ is a `SqlQuery` for retrieving a `Db.Merchant` entity
+-- with the given @name@.
+merchantByName :: Text -> SqlQuery (SqlExpr (Entity Db.Merchant))
+merchantByName name = do
+    m <- from $ table @Db.Merchant
+    where_ $ m ^. Db.MerchantName ==. val name
     pure m
 
 -- | `getMerchants` lists all known merchants.
@@ -51,15 +61,20 @@ putMerchant :: MerchantInfo -> ApiHandler (Keyed MerchantInfo)
 putMerchant MkMerchantInfo{..} = do
     -- Don't accept empty strings
     validateNonEmpty "The merchant name" merchantName
+    -- Don't accept merchants with identical names
+    let trimmedName = strip merchantName
+    validateNoEntity
+        ("A merchant named '" <> trimmedName <> "' already exists.")
+        (merchantByName trimmedName)
 
     -- Create the new merchant
     now <- liftIO getCurrentTime
     newMerchantId <- runQuery $ insert $
-        Db.Merchant merchantName now
+        Db.Merchant trimmedName now
 
     -- Return all available information about the new merchant
     pure $ mkKeyed newMerchantId $ MkMerchantInfo{
-        merchantName
+        merchantName = trimmedName
     }
 
 merchantHandlers :: ServerT MerchantsAPI ApiHandler
